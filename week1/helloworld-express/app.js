7998c4a2cf812a2a00359aaa9bc5df8924ff5dfc
Mannuel Ferreira <mannuel@wwc.co.za>
2015-08-25 22:21:12 +0200

    revert source

--- week1/helloworld-express/app.js
+++ week1/helloworld-express/app.js
@@ -1,26 +1,18 @@
 var express = require('express'),
     app = express(),
     cons = require('consolidate'),
-    Db = require('mongodb').Db,
     MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server,
-    ReplSetServers = require('mongodb').ReplSetServers,
-    ObjectID = require('mongodb').ObjectID,
-    Binary = require('mongodb').Binary,
-    GridStore = require('mongodb').GridStore,
-    Grid = require('mongodb').Grid,
-    Code = require('mongodb').Code,
-    BSON = require('mongodb').pure().BSON,
-    assert = require('assert');
+    Server = require('mongodb').Server;
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 var mongoclient = new MongoClient(new Server("localhost", 27017));
-var db = mongoclient.db('demo');
+var db = mongoclient.db('course');
 
 app.get('/', function(req, res){
+
     // Find one document in our collection
     db.collection('hello_mongo_express').findOne({}, function(err, doc) {
 
