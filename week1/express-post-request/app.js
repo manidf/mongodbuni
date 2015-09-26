788e27bb0c702b37656ec5d4fbc0b762b7b14629
Mannuel Ferreira <mannuel@wwc.co.za>
2015-09-26 11:46:15 +0200

    json objects

--- week1/express-post-request/app.js
+++ week1/express-post-request/app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
-    app = express(),
-    cons = require('consolidate');
+  app = express(),
+  cons = require('consolidate');
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
@@ -9,24 +9,24 @@ app.use(express.bodyParser());
 
 //Handler for internal server errors
 function errorHandler(err, req, res, next) {
-    console.error(err.message);
-    console.error(err.stack);
-    res.status(500);
-    res.render('error_template', { error: err });
+  console.error(err.message);
+  console.error(err.stack);
+  res.status(500);
+  res.render('error_template', { error: err });
 }
 
 app.use(errorHandler);
 
 app.get('/', function(req,res, next) {
-    res.render('fruitPicker', { 'fruits' : [ 'apple', 'orange', 'banana', 'peach', 'strawberries' ]});
+  res.render('fruitPicker', { 'fruits' : [ 'apple', 'orange', 'banana', 'peach', 'strawberries' ]});
 });
 
 app.post('/favourite_fruit', function(req, res, next) {
-    var favourite = req.body.fruit;
-    if (typeof favourite == 'undefined') {
-        next(Error('Please choose a fruit'))
-    } else {
-        res.send("your favourite fruit is " + favourite);
+  var favourite = req.body.fruit;
+  if (typeof favourite == 'undefined') {
+    next(Error('Please choose a fruit'))
+  } else {
+    res.send("your favourite fruit is " + favourite);
     }
 });
 
